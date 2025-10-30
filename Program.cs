dc57140ac5b4f75f61be441c143174e7f79c5ecb
Gustavo H. Rice <gustavo.rice@hotmail.com>
2025-10-30 08:58:52 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        static void Menu()
+        {
+            Console.WriteLine("Digite quantos segundos deseja contar: ");
+        }
+
+
+    }
+}
